4cecc7ea5ea32d6400060f4b054874dcdc25cf29
Luca Terracciano <terraccianoluca@gmail.com>
2020-06-14 15:40:33 +0200

    feat: add main and key handler stub

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Main program function
+ */
+function main() {
+
+}
+
+/**
+ * Handles the key pressed event
+ * @param {Event} e the key pressed 
+ */
+function keyHandler(e) {
+
+}
+
+//main function is the program entry point
+window.onload = main;
+
+//call the handler when after pressing a button
+window.addEventListener("keyup", keyHandler, false);
