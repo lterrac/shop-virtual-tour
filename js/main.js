11c04b77e576dfdfbe178fa953389c8c87d0246e
Luca Terracciano <terraccianoluca@gmail.com>
2020-06-15 23:46:07 +0200

    feat: add shaders stub

--- js/main.js
+++ js/main.js
@@ -3,6 +3,63 @@
  */
 function main() {
 
+	//Get canvas data
+	canvas.getCanvas();
+
+	//Initialize GLSL program
+	shaders.initProgram();
+
+	//Draw the scene
+	drawScene();
+
+}
+
+/**
+ * Draw the scene fram
+ */
+function drawScene() {
+	//Update objects matrices
+	updateTransformationMatrices();
+	
+	//Send data to GLSL program
+	shaders.bindVertexArray();
+	shaders.sendUniformsToGpU();
+
+	//Draw the objects
+	shaders.drawObjects();
+
+	//Execute the function every frame
+	window.requestAnimationFrame(drawScene);
+}
+
+/**
+ * Updates the object local and world matrices, the View and the perspetcive matrix
+ */
+function updateTransformationMatrices() {
+	updateModels();
+	updateView();
+	updatePerspective();
+}
+
+/**
+ * Update local and world matrices
+ */
+function updateModels() {
+	//#TODO
+}
+
+/**
+ * Update view matrix
+ */
+function updateView() {
+	//#TODO
+}
+
+/**
+ * Update perspective matrix
+ */
+function updatePerspective() {
+	//#TODO
 }
 
 
---
+++ js/shaders.js
@@ -0,0 +1,47 @@
+var shaders = {
+
+    initProgram: function() {
+        compileShaders();
+        linkShaders();
+        getAttributesLocation();
+        getUniformsLocation();
+        createVaos();
+        putAttributesOnGpu();
+    },
+
+    compileShaders: function() {
+        //#TODO
+    },
+
+    linkShaders: function() {
+        //#TODO
+    },
+
+    getAttributesLocation: function() {
+        //#TODO
+    },
+
+    getUniformsLocation: function() {
+        //#TODO
+    },
+
+    createVaos: function() {
+        //#TODO
+    },
+
+    putAttributesOnGpu: function() {
+        //#TODO
+    },
+
+    bindVertexArray: function() {
+        //#TODO
+    },
+
+    sendUniformsToGpU: function() {
+        //#TODO
+    },
+
+    drawObjects: function() {
+        //#TODO
+    },
+}
