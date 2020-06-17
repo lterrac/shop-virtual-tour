089966a5f589c6233b2c49b9391ab345fcfc28d6
Luca Terracciano <terraccianoluca@gmail.com>
2020-06-17 22:41:38 +0200

    feat: add object loading

--- js/models.js
+++ js/models.js
@@ -1,9 +1,50 @@
 var models = {
+    
+    furnituresName: [
+        'bed',
+    ],
+
+    furnitures: {
+        'bed': null,
+    },
+
     /**
      * Load models from file
      */
     loadModels: function () {
-        //#TODO
+        this.furnituresName.forEach( furnitureName => {
+
+            furnitureUrl = "http://127.0.0.1:8000/models/" + furnitureName + "/" + furnitureName;
+            utils.get_json(furnitureUrl + ".json",
+                            model => {
+                                //Extract model's data
+                                this.furnitures[furnitureName] = {};
+                                this.furnitures[furnitureName].model = model;
+                                this.furnitures[furnitureName].vertices = model.meshes[0].vertices;
+                                this.furnitures[furnitureName].indices = [].concat.apply([], model.meshes[0].faces)
+                                this.furnitures[furnitureName].textureCoordinates = model.meshes[0].texturecoords[0];
+                                this.furnitures[furnitureName].normals = model.meshes[0].normals;
+                                this.furnitures[furnitureName].texture = canvas.gl.createTexture();
+
+                                var texture = this.furnitures[furnitureName].texture;
+                                var gl = canvas.gl;
+
+                                //Create and set texture
+                                gl.activeTexture(gl.TEXTURE0);
+                                gl.bindTexture(gl.TEXTURE_2D, texture);
+
+                                var image = new Image();
+                                //Done for security reason
+                                image.crossOrigin = "anonymous";
+                                image.src = furnitureUrl + ".png";
+
+                                image.onload = function() {
+                                    gl.bindTexture(gl.TEXTURE_2D, texture);
+                                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+                                    gl.generateMipmap(gl.TEXTURE_2D);
+                                };
+                            });
+        });
     },
 
     /**
