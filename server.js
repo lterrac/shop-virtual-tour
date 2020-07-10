25aa1df968f64a210f3ef9e0f1ed2d552ad56e5a
Luca Terracciano <luca.terracciano@mia-platform.eu>
2020-07-10 23:02:36 +0200

    fix: change settings of server

--- server.js
+++ server.js
@@ -4,5 +4,17 @@ var app = express();
 //setting middleware
 app.use(express.static(__dirname)); //Serves resources from public folder
 
+/**
+ * Set the server port
+ */
+let serverPort = process.env.PORT || 8000;
 
-var server = app.listen(8000);
+app.set("port", serverPort);
+
+initSqlDB();
+initDb();
+
+/* Start the server on port 8000 */
+app.listen(serverPort, function() {
+    console.log(`Your app is ready at port ${serverPort}`);
+});
